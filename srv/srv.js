29af0aaee2378a8ebc79a2409c57cef1778fe1b8
Theo Sun <theo.sun@sap.com>
2021-03-01 16:01:35 +0800

    chore: update

--- srv/srv.js
+++ srv/srv.js
@@ -1,8 +1,22 @@
 const { resolve } = require("@sap/cds")
 
 
+
+/**
+ *
+ * @param {import("@sap/cds/apis/services").Service} srv
+ */
 module.exports = srv => {
-    srv.on("queryAny", async () => {
+
+    srv.on("queryAny", async (req) => {
         new Promise((resolve, reject) => { reject(new Error()) })
+    })
+
+    srv.before("READ", "Peoples", async (req) => {
+        /**
+         * @type {import("@sap/cds/apis/ql").SELECT}
+         */
+        const query = req.query
+        query.where({ Age: { ">": 99 } })
     })
 }
